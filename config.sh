6d9e8919728340e1cba7cd7609e00ddc481694c7
Peter Klijn <pjmklijn@gmail.com>
2016-09-04 10:18:05 +0200

    Made initial version of file watcher

--- config.sh
+++ config.sh
@@ -14,8 +14,21 @@ wait_for () {
 
 wait_for "http://localhost:8080/cli/"
 
-echo "\n\n-------------------\n\n"
+WOKRSPACE_PATH="/var/jenkins_home/jobs/seed/workspace/"
+
+OLD_VALUE=""
+
 while true; do
+
+  NEW_VALUE=`(ls -lFR $WOKRSPACE_PATH | grep -E '.*[^/:]$' | grep -v -E 'total \d+' | awk '{print $6 $7 $8 $9}' | tr '\n' '-')`
+
+  if [ "${OLD_VALUE}" != "${NEW_VALUE}" ]; then
+    curl -s -X POST localhost:8080/job/seed/build --user admin:admin > /dev/null
+    OLD_VALUE=$NEW_VALUE
+    echo "\nRan seed at " `date`
+  fi
+
   sleep 1
+
 done
 
